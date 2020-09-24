df397c2c8aae3db4103dc1833e190b7eb6330d2f
Ayushish93 <69399771+Ayushish93@users.noreply.github.com>
2020-09-23 18:00:00 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
+// completed with Nicholas,Josh,Ayushi
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return undefined
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = [];
+    for (let i = 0; i < letters.length; i++) {
+        let newWord = "";
+        for (let j = 0; j < letters[i].length; j++) {
+            newWord += letters[j][i];
+        }
+        verticalJoin.push(newWord);
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
